dc01e39edaa28714eacac15003ab89aebef56ffc
smart-builder <ringroseleekevin@gmail.com>
2023-11-30 14:50:30 +0000

    add Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import { Container } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import json2mq from 'json2mq';
+
+const Header = () => {
+  const matchMedia = useMediaQuery(json2mq({
+    minWidth: 612
+  }));
+  return (
+    <AppBar
+      position='fixed'
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100px',
+        background: '#f5ca16',
+        color: 'black'
+      }}>
+      <Container>
+        <Typography variant={`${matchMedia ? 'h2' : 'h4'}`} sx={{
+          fontWeight: '800'
+        }}>
+          CREDIT CROSSOVER
+        </Typography>
+      </Container>
+
+    </AppBar>
+  );
+};
+
+export default Header;
