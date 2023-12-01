84982341e45efb62d064e2c7660a0d7dcb933c9d
smart-builder <ringroseleekevin@gmail.com>
2023-12-01 17:30:05 +0000

    wrapped app with context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
+import React from 'react';
 import { Container } from '@mui/material';
+
+import ContextWrapper from './AppContext';
 import './App.css';
 import Header from './components/Header';
 import SearchBox from './components/SearchBox';
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <Container
-        role='feed'
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          pt: 5
-        }}
-      >
-        <SearchBox searchText='Bruce' />
-      </Container>
-    </div>
+    <ContextWrapper>
+      <div className='App'>
+        <Header />
+        <Container
+          role='feed'
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            pt: 5
+          }}
+        >
+          <SearchBox searchText='Bruce' />
+        </Container>
+      </div>
+    </ContextWrapper>
   );
 }
 
