46add5aa731ded12942485f7bc7f3c93604586e7
leeringrose <ringroseleekevin@gmail.com>
2023-12-20 11:01:38 +0000

    re-place papers for categories

--- src/components/Feed/Left/index.tsx
+++ src/components/Feed/Left/index.tsx
@@ -1,20 +1,36 @@
 import React from 'react';
 
+import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
 const Left: React.FC = () => {
   return (
-    <Box
-      minWidth={350}
-      p={5}
+    <Paper
+      elevation={7}
       sx={{
+        p: 4,
+        minWidth: 350,
         backgroundColor: 'white',
-        borderRadius: '3rem'
+        borderRadius: '1.3rem',
+        border: '3px solid darkgrey',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'stretch'
       }}
     >
-      <Typography variant='h3' component='div'>Welcome!</Typography>
-    </Box>
+      <Box
+        display='flex'
+        alignItems='center'
+        justifyContent='center'
+      >
+        <Typography
+          variant='h3'
+          component='div'>
+          Welcome!
+        </Typography>
+      </Box>
+    </Paper>
   );
 };
 
--- src/components/Feed/MainBoard/index.tsx
+++ src/components/Feed/MainBoard/index.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
 
 import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
 const MainBoard = () => {
-  return <Box
-    display='flex'
-    flex={1}
+  return <Paper
+    elevation={7}
+    sx={{
+      p: 4,
+      minWidth: 350,
+      backgroundColor: 'white',
+      borderRadius: '1.3rem',
+      border: '3px solid darkgrey',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'stretch'
+    }}
   >
-    <h5>Main Board</h5>
-  </Box>;
+    <Box
+      display='flex'
+      alignItems='center'
+      justifyContent='center'
+    >
+      <Typography
+        variant='h3'
+        component='div'>
+        Welcome!
+      </Typography>
+    </Box>
+  </Paper>;
 };
 
 export default MainBoard;
